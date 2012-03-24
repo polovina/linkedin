519e6dcd6d87045d3455cc53cdb1e3099148027a
saasbook <saasbook@saasbook.(none)>
2012-03-23 18:44:35 -0700

    Third-2

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,13 +1,13 @@
 scheduler = Rufus::Scheduler.start_new
  
 
- scheduler.every(Task.where(:task=>"stream")[0][:frequency]) do
+ #scheduler.every(Task.where(:task=>"stream")[0][:frequency]) do
  scheduler.every("10m") do
  Shot.stream
  end
   
  
-scheduler.every(Task.where(:task=>"search")[0][:frequency]) do
+ #scheduler.every(Task.where(:task=>"search")[0][:frequency]) do
  scheduler.every("1h") do
  Shot.search
  end
